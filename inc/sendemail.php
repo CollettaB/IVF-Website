33e1e51c8104a03443d96d0b97062f1b681bf7d6
CollettaB <Colletta.baker13@gmail.com>
2022-06-09 15:15:53 +1000

    email

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,21 +1,16 @@
 <?php
 
-    $to = "one@gmail.com";
+    $to = "colletta.baker13@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message from your Template";
+    $subject = "New Interested Viewer
 
     $fields = array();
     $fields{"firstName"}    = "First Name";
     $fields{"lastName"}    = "Last Name";
     $fields{"email"}    = "Email";
-    $fields{"phone"}    = "Phone Number";
-    $fields{"message"}   = "Message";
-    
 
-    $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
-
-    $send = mail($to, $subject, $body, $headers);
+    $send = mail($to, $subject, $headers);
 
 ?>
